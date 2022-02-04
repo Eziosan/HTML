8f9c61ce65bbe7a25f7d983b74b613653faea553
Eziosan <nmkl96@naver.com>
2022-02-04 09:37:17 +0900

    no message

--- MainProject/src/main/java/music/is/mylife/controller/HomeController.java
+++ MainProject/src/main/java/music/is/mylife/controller/HomeController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class MainController {
+public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String main(Locale locale) {
